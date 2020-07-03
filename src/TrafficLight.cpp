97aa544a7f33605069865b1880d81e0d82968ded
Thomas H. Lipscomb <nano2ba@gmail.com>
2020-07-03 21:16:26 +0000

    Task FP.5 done

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -8,26 +8,37 @@
 
 /* Implementation of class "MessageQueue" */
 
-/* 
 template <typename T>
 T MessageQueue<T>::receive()
 {
-    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
-    // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
+    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait()
+    // to wait for and receive new messages and pull them from the queue using move semantics.
+    // The received object should then be returned by the receive function.
+
+    // https://github.com/AbhishekRepos/CppND-Program-a-Concurrent-Traffic-Simulation/blob/master/src/TrafficLight.cpp
+    std::unique_lock<std::mutex> lock(_mutex);
+    _condition.wait(lock);
+    auto message{std::move(_queue.front())};
+    _queue.pop_front();
+
+    return message;
 }
 
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
-    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
+    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex>
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+  
+    // https://github.com/AbhishekRepos/CppND-Program-a-Concurrent-Traffic-Simulation/blob/master/src/TrafficLight.cpp
+    const std::lock_guard<std::mutex> lock(_mutex);
+    _queue.emplace_back(msg);
+    _condition.notify_one();
 }
-*/
+
 
 /* Implementation of class "TrafficLight" */
 
-/* 
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
@@ -38,14 +49,22 @@ void TrafficLight::waitForGreen()
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
+    // https://github.com/AbhishekRepos/CppND-Program-a-Concurrent-Traffic-Simulation/blob/master/src/TrafficLight.cpp
+    bool condition{true};
+
+    while (condition)
+    {
+        if (_trafficLightQueue.receive() == TrafficLightPhase::green)
+        {
+            condition = false;
+        }
+    }
 }
 
 TrafficLightPhase TrafficLight::getCurrentPhase()
 {
     return _currentPhase;
 }
-
-*/
 
 void TrafficLight::simulate()
 {
