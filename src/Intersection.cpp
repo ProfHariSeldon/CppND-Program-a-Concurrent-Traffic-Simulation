74a7f96fb74df2698911fd1eff2785539785f47c
Thomas H. Lipscomb <nano2ba@gmail.com>
2020-07-03 21:22:07 +0000

    Task FP.6 compile error

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -87,6 +87,11 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
     
     // FP.6b : use the methods TrafficLight::getCurrentPhase and TrafficLight::waitForGreen to block the execution until the traffic light turns green.
+    // https://github.com/AbhishekRepos/CppND-Program-a-Concurrent-Traffic-Simulation/blob/master/src/Intersection.cpp
+    if (_trafficLight.getCurrentPhase() == TrafficLightPhase::red)
+    {
+        _trafficLight.waitForGreen();
+    }
 
     lck.unlock();
 }
@@ -109,6 +114,8 @@ void Intersection::setIsBlocked(bool isBlocked)
 void Intersection::simulate() // using threads + promises/futures + exceptions
 {
     // FP.6a : In Intersection.h, add a private member _trafficLight of type TrafficLight. At this position, start the simulation of _trafficLight.
+    // https://github.com/AbhishekRepos/CppND-Program-a-Concurrent-Traffic-Simulation/blob/master/src/Intersection.cpp
+    _trafficLight.simulate();
 
     // launch vehicle queue processing in a thread
     threads.emplace_back(std::thread(&Intersection::processVehicleQueue, this));
